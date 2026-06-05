af1d4be4a7bba1cc717459b6108a90da765fd5ab
KKNOBUG <807440781@qq.com>
2026-06-05 10:58:05 +0800

    ...

--- applications/base/schemas/audit_schema.py
+++ applications/base/schemas/audit_schema.py
@@ -14,24 +14,45 @@ from pydantic import BaseModel, Field
 from enums import HTTPMethod
 
 
-class AuditCreate(BaseModel):
-    user_id: int
-    username: str
-    request_time: datetime
-    request_tags: Optional[str]
-    request_summary: Optional[str]
-    request_method: HTTPMethod
-    request_router: str
-    request_client: Optional[str]
-    request_header: Optional[dict]
-    request_params: Optional[str]
-    response_time: datetime
-    response_header: Optional[dict]
-    response_code: Optional[str]
-    response_message: Optional[str]
-    response_params: Optional[str]
-    response_elapsed: str
+class AuditBase(BaseModel):
+    user_id: Optional[int] = Field(default=None, description="用户ID")
+    username: Optional[str] = Field(default=None, max_length=32, description="用户名称")
+    request_time: Optional[datetime] = Field(default=None, description="请求时间")
+    request_tags: Optional[str] = Field(default=None, max_length=255, description="请求模块")
+    request_summary: Optional[str] = Field(default=None, max_length=255, description="请求接口")
+    request_method: Optional[HTTPMethod] = Field(default=None, description="请求方式")
+    request_router: Optional[str] = Field(default=None, max_length=255, description="请求路由")
+    request_client: Optional[str] = Field(default=None, max_length=16, description="请求来源")
+    request_header: Optional[dict] = Field(default=None, description="请求头部")
+    request_params: Optional[str] = Field(default=None, description="请求参数")
+    response_time: Optional[datetime] = Field(default=None, description="响应时间")
+    response_header: Optional[dict] = Field(default=None, description="响应头部")
+    response_code: Optional[str] = Field(default=None, max_length=16, description="响应代码")
+    response_message: Optional[str] = Field(default=None, max_length=512, description="响应消息")
+    response_params: Optional[str] = Field(default=None, description="响应参数")
+    response_elapsed: Optional[str] = Field(default=None, max_length=16, description="响应耗时")
+
+
+class AuditCreate(AuditBase):
+    user_id: int = Field(..., description="用户ID")
+    username: str = Field(..., max_length=32, description="用户名称")
+    request_time: datetime = Field(..., description="请求时间")
+    request_method: HTTPMethod = Field(..., description="请求方式")
+    request_router: str = Field(..., max_length=255, description="请求路由")
+    response_time: datetime = Field(..., description="响应时间")
+    response_elapsed: str = Field(..., max_length=16, description="响应耗时")
+
+    def create_dict(self):
+        return self.model_dump(exclude_unset=True)
+
+
+class AuditSelect(AuditBase):
+    page: int = Field(default=1, ge=1, description="页码")
+    page_size: int = Field(default=10, ge=10, description="每页数量")
+    order: Optional[list] = Field(default=[], examples=["-created_time"], description="排序字段")
+    start_time: Optional[str] = Field(default=None, description="开始时间")
+    end_time: Optional[str] = Field(default=None, description="结束时间")
 
 
 class AuditBatchDelete(BaseModel):
-    audit_ids: Optional[List[int]] = Field(None, description="审计日志ID列表")
+    audit_ids: Optional[List[int]] = Field(default=None, description="审计日志ID列表")
--- applications/base/views/audit_view.py
+++ applications/base/views/audit_view.py
@@ -12,7 +12,7 @@ from fastapi import APIRouter, Body, Query
 from tortoise.expressions import Q
 
 from applications.base.models.audit_model import Audit
-from applications.base.schemas.audit_schema import AuditBatchDelete
+from applications.base.schemas.audit_schema import AuditBatchDelete, AuditSelect
 from applications.base.services.audit_crud import AUDIT_CRUD
 from configure import LOGGER
 from core.responses import FailureResponse, SuccessResponse
@@ -20,18 +20,18 @@ from core.responses import FailureResponse, SuccessResponse
 audit = APIRouter()
 
 
-@audit.get("/list", summary="查看操作日志")
+@audit.get("/list", summary="查看操作日志", description="支持分页按条件查询审计日志列表信息（Query）")
 async def list_audit(
         page: int = Query(default=1, ge=1, description="页码"),
         page_size: int = Query(default=10, ge=10, description="每页数量"),
-        username: str = Query(default="", description="用户名称"),
-        request_tags: str = Query(default="", description="请求模块"),
-        request_summary: str = Query(default="", description="请求接口"),
-        request_method: str = Query(default="", description="请求方式"),
-        request_router: str = Query(default="", description="请求路由"),
-        response_code: str = Query(default="", description="响应代码"),
-        start_time: str = Query(default="", description="开始时间"),
-        end_time: str = Query(default="", description="结束时间"),
+        username: str = Query(default=None, description="用户名称"),
+        request_tags: str = Query(default=None, description="请求模块"),
+        request_summary: str = Query(default=None, description="请求接口"),
+        request_method: str = Query(default=None, description="请求方式"),
+        request_router: str = Query(default=None, description="请求路由"),
+        response_code: str = Query(default=None, description="响应代码"),
+        start_time: str = Query(default=None, description="开始时间"),
+        end_time: str = Query(default=None, description="结束时间"),
 ):
     q = Q()
     if username:
@@ -54,6 +54,34 @@ async def list_audit(
         q &= Q(created_time__lte=end_time)
 
     audit_log_objs = await Audit.filter(q).offset((page - 1) * page_size).limit(page_size).order_by("-created_time")
+    total = await Audit.filter(q).count()
+    data = [await audit_log.to_dict() for audit_log in audit_log_objs]
+    return SuccessResponse(data=data, total=total)
+
+
+@audit.post("/search", summary="查询操作日志", description="支持分页按条件查询审计日志列表信息（Body）")
+async def search_audit(user_in: AuditSelect = Body()):
+    q = Q()
+    if user_in.username:
+        q &= Q(username__icontains=user_in.username)
+    if user_in.request_tags:
+        q &= Q(request_tags__icontains=user_in.request_tags)
+    if user_in.request_summary:
+        q &= Q(request_summary__icontains=user_in.request_summary)
+    if user_in.request_method:
+        q &= Q(request_method__icontains=user_in.request_method)
+    if user_in.request_router:
+        q &= Q(request_router__icontains=user_in.request_router)
+    if user_in.response_code:
+        q &= Q(response_code__icontains=user_in.response_code)
+    if user_in.start_time and user_in.end_time:
+        q &= Q(created_time__range=[user_in.start_time, user_in.end_time])
+    elif user_in.start_time:
+        q &= Q(created_time__gte=user_in.start_time)
+    elif user_in.end_time:
+        q &= Q(created_time__lte=user_in.end_time)
+
+    audit_log_objs = await Audit.filter(q).offset((user_in.page - 1) * user_in.page_size).limit(user_in.page_size).order_by(*user_in.order or ["-created_time"])
     total = await Audit.filter(q).count()
     data = [await audit_log.to_dict() for audit_log in audit_log_objs]
     return SuccessResponse(data=data, total=total)
